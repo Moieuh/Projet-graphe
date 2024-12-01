8165d62f62eedeeff83056d98bce5e9cfb85ed70
Gilva <1281656346.z@gmail.com>
2024-12-01 11:57:32 +0100

    étape3

--- main.py
+++ main.py
@@ -10,4 +10,23 @@ print_graphe_ordonancement(tableau)
 if tableau:
     n = len(tableau)  # Nombre de tâches
     matrice = construire_matrice(tableau, n)
+    
     print_matrice(matrice)
+ 
+    test_circuit=circuits(matrice)
+    
+
+    if test_circuit :
+        print("Il y a un circuit ")
+        print("C'est n'est pas un graphe d'ordonnancement")
+        
+    else :
+        print("Il n'y a pass de circuit ")
+        test_arc=arc_neg(matrice)
+        if test_arc ==False:
+            print("Il n'y a pas d'arcs négatifs")
+            print("C'est un graphe d'ordonnancement")
+        else:
+            print("Il a des arcs négatifs")
+            print("C'est n'est pas un graphe d'ordonnancement")
+            
